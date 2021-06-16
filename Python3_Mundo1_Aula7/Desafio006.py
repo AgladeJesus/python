bcbf2c456c4f129f6ac6e99b52e2972d1c3c4746
AgladeJesus <agladejesus@gmail.com>
2021-06-16 06:18:40 -0400

    Create Desafio006.py

---
+++ Python3_Mundo1_Aula7/Desafio006.py
@@ -0,0 +1,5 @@
+a = int(input('Digite um número: '))
+b = a * 2
+c = a * 3
+d = a ** (1/2)
+print('O número digitado foi: {}, o seu dobro é: {}, o seu triplo é: {}, e a raiz quadrada é :{:.3f}'.format(a, b, c, d))
