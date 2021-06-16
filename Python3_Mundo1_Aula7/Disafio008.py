d45c3680aa8e9a648c7015bf1f0a402d609fa44d
AgladeJesus <agladejesus@gmail.com>
2021-06-16 12:33:47 -0400

    Create Disafio008.py

---
+++ Python3_Mundo1_Aula7/Disafio008.py
@@ -0,0 +1,8 @@
+a = int(input('Digite uma medida em metros: '))
+
+b = a * 100
+c = a * 1000
+
+print('A medida informarda foi: {}m, \nConvertendo essa medida para centimetros é: {}cm,\nConvertendo essa medida para milimetros é: {}mm'. format(a, b, c))
+
+
