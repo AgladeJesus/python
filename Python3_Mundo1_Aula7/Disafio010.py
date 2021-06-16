8cd973aaf667b44bfcb73736311e3def13cea612
AgladeJesus <agladejesus@gmail.com>
2021-06-16 13:04:41 -0400

    Create Disafio010.py

---
+++ Python3_Mundo1_Aula7/Disafio010.py
@@ -0,0 +1,6 @@
+cart = float(input('Qual valor você tem na carteira? '))
+usd_hj = float(input('Digite o valor do dálar de hoje: '))
+conv = cart // usd_hj
+rest = cart % usd_hj
+
+print('Valor que tenho na Carteira é: R$ {} \nValor de dólar de hoje é: R$ {} \nCom o que tenho consigo comprar USD {} \n E Sobrará R$ {} de Troco'. format(cart, usd_hj, conv, rest))
