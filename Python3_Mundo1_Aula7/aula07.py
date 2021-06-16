6a88196bd5916655682ef4dd31834940c5269b24
AgladeJesus <agladejesus@gmail.com>
2021-06-16 05:13:43 -0400

    updata aula07.py

---
+++ Python3_Mundo1_Aula7/aula07.py
@@ -0,0 +1,11 @@
+n1 = int(input('um Valor: '))
+n2 = int(input('Outro Valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+sb = n1 - n2
+rest = n1 % n2
+print('A soma é: {}, a multiplicação é: {}, a divisão é: {:.3f}'.format(s,m,d))
+print('Divisão inteira é: {}, a Esponeciação é: {}, a Subtração é: {}, e o resto da divisão é: {}'.format(di,e,sb,rest))
