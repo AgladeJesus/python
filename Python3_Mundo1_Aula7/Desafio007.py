d926e3c2cbc48dd6fa566719f44a70e2af543830
AgladeJesus <agladejesus@gmail.com>
2021-06-17 12:12:08 -0400

    Update Desafio007.py

--- Python3_Mundo1_Aula7/Desafio007.py
+++ Python3_Mundo1_Aula7/Desafio007.py
@@ -2,6 +2,6 @@ aluno = str(input('Nome do Aluno: '))
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input("Digite a segunda nota: "))
 media = (nota1 + nota2) / 2
-print('Aluno: {} \n Primeira nota: {} \n Segunda nota: {}\n Média do aluno: {}'.format(aluno, nota1, nota2, media))
+print('Aluno: {} \n Primeira nota: {:.1f} \n Segunda nota: {:.1f}\n Média do aluno: {:.1f}'.format(aluno, nota1, nota2, media))
 
 print('Obrigado')
