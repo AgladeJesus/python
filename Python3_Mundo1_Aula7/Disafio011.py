31ad6860f1d6b7305c1db8937c23584173a2d4f2
AgladeJesus <agladejesus@gmail.com>
2021-06-17 08:30:53 -0400

    Create Disafio011.py

---
+++ Python3_Mundo1_Aula7/Disafio011.py
@@ -0,0 +1,13 @@
+larg = float(input('Digite a largura da Parede a ser pintada: '))
+alt = float(input('Digite a altura da parede a ser pintada: '))
+lt = float(2)
+res = float(((larg*alt) / lt))
+
+print('=' * 50)
+print('CALCULO DE TINTA À SER USADA PARA PINTURA DE PAREDE')
+print('=' * 50)
+print('Largura: {}'.format(larg))
+print('Altura: {}'.format(alt))
+print('Aréa total: {}'.format(larg * alt))
+print('Será necessario de: {} litros de tinta'.format(res))
+print('=' * 20)
