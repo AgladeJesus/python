ab0aed6f617926a5c6fe42f597f151b9b07abf9a
AgladeJesus <agladejesus@gmail.com>
2021-06-17 12:50:46 -0400

    Update Desafio010.py

--- Python3_Mundo1_Aula7/Desafio010.py
+++ Python3_Mundo1_Aula7/Desafio010.py
@@ -1,6 +1,5 @@
 cart = float(input('Qual valor você tem na carteira? '))
-usd_hj = float(input('Digite o valor do dálar de hoje: '))
-conv = cart // usd_hj
-rest = cart % usd_hj
+usd_hj = 3.27
+conv = cart / usd_hj
 
-print('Valor que tenho na Carteira é: R$ {} \nValor de dólar de hoje é: R$ {} \nCom o que tenho consigo comprar USD {} \n E Sobrará R$ {:.2f} de Troco'. format(cart, usd_hj, conv, rest))
+print('Valor que tenho na Carteira é: R$ {} \nValor de dólar de hoje é: R$ {:.2f} \nCom o que tenho consigo comprar USD {:.2f}'. format(cart, usd_hj, conv))
