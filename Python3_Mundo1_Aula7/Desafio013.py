a41c2cc276e1edee33104ba8677dc9be6b3abf92
AgladeJesus <agladejesus@gmail.com>
2021-06-18 04:36:51 -0400

    Update Desafio013.py

--- Python3_Mundo1_Aula7/Desafio013.py
+++ Python3_Mundo1_Aula7/Desafio013.py
@@ -1,5 +1,5 @@
 sal = float(input('Salário: '))
 perc_aum = float(input('Percentual de aumento: '))
-aum = sal * ((perc_aum/100)+1)
+aum = sal * ((perc_aum / 100) + 1)
 
 print('O novo Salário será: R${:.2f}'.format(aum))
