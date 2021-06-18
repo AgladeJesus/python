44c256bb822b22e83a7c30c43dd134980cc3a9c0
AgladeJesus <agladejesus@gmail.com>
2021-06-18 12:35:17 -0400

    Create Desafio016

---
+++ Python3_Mundo1_Aula8/Desafio016.py
@@ -0,0 +1,7 @@
+'''from math import trunc
+n = float(input('Digite um número: '))
+print('Número digitado {}'.format(n))
+print('A parte inteira do número digitado é: {}'.format(trunc(n)))'''
+
+n = float(input('Digite um número: '))
+print('A parte inteira do seguinte número {}, é {}'.format(n, int(n)))
