8a46d1ed2104e35788410bb63d184e56db86e9a6
AgladeJesus <agladejesus@gmail.com>
2021-06-18 13:00:07 -0400

    Create Desafio017.py

---
+++ Python3_Mundo1_Aula8/Desafio017.py
@@ -0,0 +1,4 @@
+co = float(input('comprimento do catesto oposto: '))
+ca = float(input('Compromento do cateto adjacente: '))
+h1 = (co ** 2 + ca ** 2) ** (1/2)
+print('O comprimento da hipotenusa é: {:.2f}'.format(h1))
