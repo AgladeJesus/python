61b4bf29b76502b9b41bf1cfb79d3b54f150b7d8
AgladeJesus <agladejesus@gmail.com>
2021-06-21 13:19:40 -0400

    Update Desafio018.py

--- Python3_Mundo1_Aula8/Desafio018.py
+++ Python3_Mundo1_Aula8/Desafio018.py
@@ -1,9 +1,9 @@
-import math
+from math import radians, sin, cos, tan
 
 ang = float(input('Digite um ângulo: '))
-sn = math.sin(math.radians(ang))
-csn = math.cos(math.radians(ang))
-tgt = math.tan(math.radians(ang))
+sn = sin(radians(ang))
+csn = cos(radians(ang))
+tgt = tan(radians(ang))
 
 print('O ângulo de {}º tem Seno de {:.2f}'. format(ang, sn))
 print('O ângulo de {}º tem Coseno de {:.2f}'. format(ang, csn))
