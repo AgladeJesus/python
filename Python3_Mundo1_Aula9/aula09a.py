8edc91494fb1d58f81e6070f3396650fd59e8cc7
AgladeJesus <agladejesus@gmail.com>
2021-06-22 06:23:34 -0400

    Update aula09a.py

--- Python3_Mundo1_Aula9/aula09a.py
+++ Python3_Mundo1_Aula9/aula09a.py
@@ -1,2 +1,12 @@
 frase = 'Jesus Cristo é o meu Senhor, dono e Deus!'
-print(frase)
+print(frase[:13])
+print(frase[6:13])
+print(frase[1:5:3])
+print(frase.count('s'))
+print(frase.upper().count('N'))
+print(frase.replace('Jesus', 'Yeshua'))
+print('Senhor' in frase)
+print(frase.lower().find('senhor'))
+div = frase.split()
+
+print(div[0][:13])
