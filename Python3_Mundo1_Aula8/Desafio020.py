aa105183409895c554eae70482bc545f7be2eb51
AgladeJesus <agladejesus@gmail.com>
2021-06-22 04:18:26 -0400

    Create Desafio020.py

---
+++ Python3_Mundo1_Aula8/Desafio020.py
@@ -0,0 +1,11 @@
+import random
+
+n1 = str(input('Primeira pessoa: '))
+n2 = str(input('Segunda pessoa: '))
+n3 = str(input('Terceira pessoa: '))
+n4 = str(input('Quarta pessoa: '))
+
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem de apresetação será')
+print(lista)
