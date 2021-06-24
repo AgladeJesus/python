c8e20c804faec7f648e0bfa4b67afe5e88acd598
AgladeJesus <agladejesus@gmail.com>
2021-06-24 07:50:29 -0400

    Update Desafio027.py

--- Python3_Mundo1_Aula9/Desafio027.py
+++ Python3_Mundo1_Aula9/Desafio027.py
@@ -1,4 +1,4 @@
 nome = str(input('Digite seu nome completo: ')).strip().upper()
 div = nome.split()
 print('Seu primeiro nome é: {}'.format(div[0]))
-print('Seu ultimo nome é: {}'.format(div[len(nome)-1]))
+print('Seu ultimo nome é: {}'.format(div[len(div)-1]))
