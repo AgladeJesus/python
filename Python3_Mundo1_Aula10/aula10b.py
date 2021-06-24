eb5c7065645196d2a32238d341340676ca259857
AgladeJesus <agladejesus@gmail.com>
2021-06-24 12:54:07 -0400

    Create aula 10

---
+++ Python3_Mundo1_Aula10/aula10b.py
@@ -0,0 +1,6 @@
+nome = str(input('Qual o seu nome: ')).lower
+
+if nome == 'Jesus Cristo':
+    print('É o filho de Deus, o todo poderoso, o quem pertence toda honra e toda glória!')
+else:
+    print('Aceite Jesus Cristo como Senhor e Salvador, e serás Salvo!')
