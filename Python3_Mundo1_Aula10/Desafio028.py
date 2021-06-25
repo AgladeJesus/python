c55c00409bfc8dcaf02ab692232931d725f8fc9b
AgladeJesus <agladejesus@gmail.com>
2021-06-25 04:23:50 -0400

    Create Desafio028.py

---
+++ Python3_Mundo1_Aula10/Desafio028.py
@@ -0,0 +1,12 @@
+import random
+
+rdpc = random.randrange(0, 5)
+nm = int(input('Adivinhe qual número que o PC pensou: '))
+
+if nm == rdpc:
+    print('PARABÉNS, VOCÊ ACETOU!')
+else:
+    print('TENTE NOVAMENTE, O NÚMERO NÃO É ESSE.')
+
+print(rdpc)
+
