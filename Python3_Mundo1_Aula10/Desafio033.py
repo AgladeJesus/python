b4e6cacf23e306bfe57a8b5bb8a23705c155a00b
AgladeJesus <agladejesus@gmail.com>
2021-06-25 05:52:44 -0400

    Create Desafio033.py

---
+++ Python3_Mundo1_Aula10/Desafio033.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+
+print('O maior número é: {} \nO menor número é: {}'.format(max(n1, n2, n3), min(n1, n2, n3)))
