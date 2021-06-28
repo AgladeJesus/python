16a23ccf70745c027ccee681b04c223baf3fafc2
AgladeJesus <agladejesus@gmail.com>
2021-06-28 05:12:52 -0400

    Update Desafio033b.py

---
+++ Python3_Mundo1_Aula10/Desafio033b.py
@@ -0,0 +1,20 @@
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+n3 = int(input('Digite o terceiro valor: '))
+
+#Verificando quem é menor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+#Verificando quem é maior
+
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
