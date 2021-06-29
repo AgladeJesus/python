62a7b289daf62aa4f0d91a536108de202bab2fc9
AgladeJesus <agladejesus@gmail.com>
2021-06-29 04:53:30 -0400

    Update Desafio005.py

--- Python3_Mundo1_Aula7/Desafio005.py
+++ Python3_Mundo1_Aula7/Desafio005.py
@@ -1,2 +1,2 @@
 a = int(input('Digite um número: '))
-print('O número digitado foi: {}, o seu antecessor é: {} e o seu sucessor é: {}'.format(a,(a-1),(a+1)) )
+print('O número digitado foi: \033[43m{}\033[m, o seu antecessor é: \033[44m{}\033[m e o seu sucessor é: \33[46m{}\033[m'.format(a,(a-1),(a+1)) )
