d81cda1339c31a75590a5a4ca15d5732054ed765
AgladeJesus <agladejesus@gmail.com>
2021-06-29 04:40:44 -0400

    Update Desafio02.py

--- Python3_Mundo1_Aula4/script-python/Desafio02.py
+++ Python3_Mundo1_Aula4/script-python/Desafio02.py
@@ -1,3 +1,9 @@
+cor = {'limpa':'\33[m',
+       'red':'\33[31m',
+       'bw':'\033[7;30m',
+       'azul':'\033[34m',
+       'amarelo':'\033[33m'}
+
 print('========DESAFIO 2 ========')
 print('DATA DE NASCIMENTO')
 
@@ -5,4 +11,4 @@ dia = input('Dia: ')
 mes = input('Mês: ')
 ano = input('ano: ')
 
-print('Você nasceu no dia ', dia, 'de ', mes, 'de ', ano, '. Correto?')
+print('Você nasceu no dia {}{}{} de {}{}{} de {}{}{} Correto?'.format(cor['red'],dia, cor['limpa'],cor['azul'],mes, cor['limpa'],cor['amarelo'],ano, cor['limpa']))
