90d47d0b72d87985785c823fc62d6353e90e7238
AgladeJesus <agladejesus@gmail.com>
2021-06-30 04:48:49 -0400

    Update aula12.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/aula12.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/aula12.py
@@ -1,1 +1,8 @@
-nome
+nome = str(input('Qual é su nome: '))
+if nome == 'Alexandre':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil.')
+else:
+    print('Seu nome é bem normal.')
+print('Tenha um bom dia, '.format(nome))
