5dbed0dfde91ac298f79dccf249903d790114993
AgladeJesus <agladejesus@gmail.com>
2021-06-30 06:35:07 -0400

    Create Desafio038.py

---
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio038.py
@@ -0,0 +1,9 @@
+nm1 = int(input('Primeiro número: '))
+nm2 = int(input('Segundo número: '))
+
+if nm1 > nm2:
+    print('O primeiro número é maior.')
+elif nm2 > nm1:
+    print('O segundo número é maior.')
+else:
+    print('Não existe número maior, pois eles são iguais')
