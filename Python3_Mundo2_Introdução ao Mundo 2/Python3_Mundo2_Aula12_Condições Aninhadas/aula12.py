236e6dee8e0dbd4c2310ced94757c5e060457482
AgladeJesus <agladejesus@gmail.com>
2021-06-30 05:02:21 -0400

    Update aula12.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/aula12.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/aula12.py
@@ -3,6 +3,10 @@ if nome == 'Alexandre':
     print('Que nome bonito!')
 elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
     print('Seu nome é bem popular no Brasil.')
+elif nome in 'Ana Cláudia Jéssica Juliana':
+    print('Belo nome feminino')
+elif nome == 'Arthur' or 'Gleiciane' or 'Lohanny':
+    print('Esse nome compõem a família Agla de Jesus')
 else:
     print('Seu nome é bem normal.')
-print('Tenha um bom dia, '.format(nome))
+print('Tenha um bom dia,'.format(nome))
