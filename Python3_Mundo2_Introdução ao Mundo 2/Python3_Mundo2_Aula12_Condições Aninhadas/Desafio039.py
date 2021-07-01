9d904423aee7bfa13c113561c2bd22dcf65f35ef
AgladeJesus <agladejesus@gmail.com>
2021-07-01 05:01:07 -0400

    Update Desafio039.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio039.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio039.py
@@ -1,6 +1,14 @@
-import datetime
+from datetime import date
+
 anoNasc = int(input('Digite o ano do seu Nascimento: '))
 
-ano = datetime.date.today().year
+ano = date.today().year
 idade = ano - anoNasc
-print(idade)
+
+if idade < 18:
+    print('Quando completar 18 anos, procure os serviços de alistamento militar')
+elif idade == 18:
+    print('Você pode se alistar')
+else:
+    print('Apresente-se aos serviços militar para dar baixo, pois você passou o tempo de se alistar')
+
