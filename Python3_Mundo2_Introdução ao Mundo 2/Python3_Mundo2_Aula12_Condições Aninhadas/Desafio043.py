6e4b72361b121422fa73eb56a94583be00a5a7ab
AgladeJesus <agladejesus@gmail.com>
2021-07-05 17:34:26 -0400

    Create Desafio043.py

---
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio043.py
@@ -0,0 +1,14 @@
+print('-='*20)
+print('CALCULADORA DE IMC')
+print('-='*20)
+
+peso = float(input('Qual o seu peso: '))
+altura = float(input('Qual sua altura: '))
+
+imc = ()
+
+if imc <= 18.5:
+    print('Abaixo do Peso')
+elif imc > 18.5 and imc <= 25:
+    print('Peso ideal')
+elif im
