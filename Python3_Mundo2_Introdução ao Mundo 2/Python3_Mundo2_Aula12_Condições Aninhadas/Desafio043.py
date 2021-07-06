d5f11cfb232d7f3046777748126b63f3f235ed1b
AgladeJesus <agladejesus@gmail.com>
2021-07-06 04:15:53 -0400

    Update Desafio043.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio043.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio043.py
@@ -5,10 +5,15 @@ print('-='*20)
 peso = float(input('Qual o seu peso: '))
 altura = float(input('Qual sua altura: '))
 
-imc = ()
+imc = (peso / (altura * altura))
 
-if imc <= 18.5:
-    print('Abaixo do Peso')
-elif imc > 18.5 and imc <= 25:
-    print('Peso ideal')
-elif im
+if imc < 18.5:
+    print('MAGREZA COM IMC {:.2f}'.format(imc))
+elif imc >= 18.5 and imc < 25:
+    print('NORMAL COM IMC {:.2f}'.format(imc))
+elif imc >= 25 and imc < 30:
+    print('SOBREPESO COM IMC {:.2f}'.format(imc))
+elif imc >= 30 and imc < 40:
+    print('OBESIDADE COM IMC {:.2f}'.format(imc))
+else:
+    print('OBESIDADE MÓRBIDA COM IMC {:.2f}'.format(imc))
