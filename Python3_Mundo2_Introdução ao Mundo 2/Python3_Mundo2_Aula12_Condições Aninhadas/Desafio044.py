d6aa6751c244cd64ac41b74213bdcdd9cca4eb7c
AgladeJesus <agladejesus@gmail.com>
2021-07-06 04:50:02 -0400

    Create Desafio044.py

---
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio044.py
@@ -0,0 +1,26 @@
+print('-='*20)
+print('CONDIÇÕES DE PAGAMENTO')
+print('-='*20)
+
+print('-='*20)
+print('CONDIÇÕES DE PAGTO')
+print('-='*20)
+print('[1] À vista no dinheiro ou cheque, 10% de desconto.')
+print('[2] À vista no Cartão, 5% de desconto.')
+print('[3] Parcelado em 2 vezes sem juros.')
+print('[4] Parcelado em 3 vezes com 20% de juros.\n\n')
+
+vlr = float(input('Valor total da sua compra: '))
+cp = int(input('Qual condição de Pagto você prefere? '))
+
+if cp = 1:
+    print('Valor final a Pagar: {:.2f}'.format(vlr * ((10/100)+1)))
+elif cp = 2:
+    print('Valor final a Pagar: {:.2f}'.format(vlr * ((5/100)+1)))
+elif cp = 3:
+    print('Valor final a Pagar: 2 parcelas de {:.2f}'.format(vlr / 2))
+elif cp = 4:
+    print('Valor final a Pagar: 3 parcelas de {:.2f}'.format((vlr * ((20/100)-1))/2))
+
+
+
