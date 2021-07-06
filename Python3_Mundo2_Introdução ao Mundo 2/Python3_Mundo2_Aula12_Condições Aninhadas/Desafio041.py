4510f1a51df3d994eda438899f6fab24ae44f542
AgladeJesus <agladejesus@gmail.com>
2021-07-06 16:42:56 -0400

    Update Desafio041.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio041.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio041.py
@@ -8,11 +8,11 @@ print('O atleta tem {} anos'.format(idade))
 
 if idade <= 9:
     print('Classificação: MIRIM')
-elif idade > 9 and idade <= 14:
+elif idade <= 14:
     print('Classificação: INFANTIL')
-elif idade > 14 and idade <= 19:
+elif idade <= 19:
     print('Classificação: JUNIOR')
-elif idade > 19 and idade <= 20:
+elif idade <= 25:
     print('Classificação: SÊNIOR')
 else:
     print('Classificação: MASTER')
