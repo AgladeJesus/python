c1ebb1c583b724167d394cb44535d0f3b67ca3b3
AgladeJesus <agladejesus@gmail.com>
2021-07-07 05:17:16 -0400

    Update Desafio044.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio044.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio044.py
@@ -1,10 +1,7 @@
 print('-='*20)
-print('CONDIÇÕES DE PAGAMENTO')
+print('{:=^20}'.format('CONDIÇÕES DE PAGAMENTO'))
 print('-='*20)
 
-print('-='*20)
-print('CONDIÇÕES DE PAGTO')
-print('-='*20)
 print('[1] À vista no dinheiro ou cheque, 10% de desconto.')
 print('[2] À vista no Cartão, 5% de desconto.')
 print('[3] Parcelado em 2 vezes sem juros.')
@@ -13,6 +10,7 @@ print('[4] Parcelado em 3 vezes com 20% de juros.\n\n')
 vlr = float(input('Valor total da sua compra: '))
 cp = int(input('Qual condição de Pagto você prefere? '))
 
+
 if cp == 1:
     print('Valor final a Pagar: R${:.2f}'.format(abs(vlr * ((10/100)-1))))
 elif cp == 2:
@@ -20,7 +18,10 @@ elif cp == 2:
 elif cp == 3:
     print('Valor final a Pagar: 2 parcelas de R${:.2f}'.format(vlr / 2))
 elif cp == 4:
-    print('Valor final a Pagar: 3 parcelas de R${:.2f}'.format((vlr * ((20/100)+1))/3))
+    parc = int(input('Quantas Parcelas: '))
+    print('Valor final a Pagar: {} parcelas de R${:.2f}'.format(parc, (vlr * ((20/100)+1))/parc))
+else:
+    print('Essa não é uma forma de pagto valida.')
 
 
 
