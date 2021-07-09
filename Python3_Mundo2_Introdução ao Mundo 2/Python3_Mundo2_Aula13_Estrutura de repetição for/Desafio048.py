bf0f408c5481164b213c1118211e31311ff301d2
AgladeJesus <agladejesus@gmail.com>
2021-07-09 17:18:02 -0400

    Update Desafio048.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula13_Estrutura de repetição for/Desafio048.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula13_Estrutura de repetição for/Desafio048.py
@@ -2,6 +2,7 @@ s = 0
 cont = 0
 for c in range(1, 501, 2):
     if c % 3 == 0:
-        s += c
-        cont += 1
-print('A soma de todos os {} valores são {}'.format(cont, c))
+        s = s + c
+        cont = cont + 1
+print('A soma de todos os {} valores são {}'.format(cont, s))
+
