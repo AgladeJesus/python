d1cf75c5a9e6829144bab09a27dc844e0e10877f
AgladeJesus <agladejesus@gmail.com>
2021-07-09 13:17:13 -0400

    Update Desafio047.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula13_Estrutura de repetição for/Desafio047.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula13_Estrutura de repetição for/Desafio047.py
@@ -1,3 +1,11 @@
+from time import sleep
+
 for c in range(2, 51, 2):
+    sleep(0.5)
     print('.', end=' ')
-    print(c, end=' ')
+    print(c, end=' ',)
+
+'''for c in range(2, 51):
+    if c % 2 ==0:
+        print('-', end=' ')
+        print(c, end=' ')'''
