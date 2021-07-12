6bc597ae8c3783f29f2663bcf60fa97db0712cc3
AgladeJesus <agladejesus@gmail.com>
2021-07-12 15:37:42 -0400

    Update ex001.py

--- Exemplos/ex001.py
+++
@@ -1,7 +0,0 @@
-n='Jose'
-i=25
-
-print('Você se chama {n} e tem {i} anos de idade')
-print('Você se chama () e tem () anos de idade'.format(n, i))
-print('Você se chama [] e tem [] anos de idade'.format(n, i))
-print('Você se chama {} e tem {} anos de idade'.format(n, i))
