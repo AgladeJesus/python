a2f720b2b2edaf708b1045b4f70fe0c9f9c7fd5b
AgladeJesus <agladejesus@gmail.com>
2021-07-12 10:33:03 -0400

    Update Desafio053.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula13_Estrutura de repetição for/Desafio053.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula13_Estrutura de repetição for/Desafio053.py
@@ -1,14 +1,11 @@
 frase = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split()
-print(palavras)
 junto = ''.join(palavras)
-
 inverso = ''
-for letra in range(len(junto) -1, -1, -1):
+for letra in range(len(junto)-1, -1, -1):
     inverso += junto[letra]
 if inverso == junto:
     print('Temos um palíndromo!')
 else:
     print('A frase digitada não é um palíndromo')
 print('A frese digitada é {}, e o contrario foi {}'.format(junto, inverso))
-print(letra)
