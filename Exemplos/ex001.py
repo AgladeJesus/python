a409143db915a20c2b2c732ecb1cf35cd1ba8071
AgladeJesus <agladejesus@gmail.com>
2021-07-12 16:10:45 -0400

    Update ex001.py

--- Exemplos/ex001.py
+++ Exemplos/ex001.py
@@ -2,6 +2,14 @@ print('-='*20)
 print('CALCULADORA DE PAPEL')
 print('-='*20)
 
+print('''
+[1] CONVERTER FOLHAS PARA QUILLOS
+[2] CONVERTER QUILOS PARA FOLHAS
+[3] CONVERTER FOLHAS PARA METRO LINEAR
+[4] CONVERTER QUILOS PARA METRO LINEAR
+[5] CALCULAR MICRO-ONDULADO FACE SIMPLES
+''')
+
 tipo = str(input('Digite o tipo de Substrato: ')).upper()
 qtdefls = int(input('Quantidade de folhas: '))
 larg = int(input('Largura: '))
