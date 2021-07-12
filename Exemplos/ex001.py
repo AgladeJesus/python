62fd789304088754e9a12e078a834a5e891ef67f
AgladeJesus <agladejesus@gmail.com>
2021-07-12 16:30:22 -0400

    Update ex001.py

--- Exemplos/ex001.py
+++ Exemplos/ex001.py
@@ -1,7 +1,6 @@
 print('-='*20)
 print('CALCULADORA DE PAPEL')
 print('-='*20)
-
 print('''
 [1] CONVERTER FOLHAS PARA QUILLOS
 [2] CONVERTER QUILOS PARA FOLHAS
@@ -9,14 +8,23 @@ print('''
 [4] CONVERTER QUILOS PARA METRO LINEAR
 [5] CALCULAR MICRO-ONDULADO FACE SIMPLES
 ''')
+def calc_peso_folha():
+    larg = int(input('Largura: '))
+    comp = int(input('Comprimento: '))
+    gram = int(input('Gramatura: '))
 
-tipo = str(input('Digite o tipo de Substrato: ')).upper()
-qtdefls = int(input('Quantidade de folhas: '))
-larg = int(input('Largura: '))
-comp = int(input('Comprimento: '))
-gram = int(input('Gramatura: '))
+    peso = ((larg/1000)*(comp/1000)*(gram/1000))
+    return peso
 
-print('Descrição: Papel {}, nas dimenssões {}mm x {}mm - {}g para quantidade: {} folhas.'.format(tipo, larg, comp, gram, qtdefls))
+opção = int(input('O quer você deseja fazer: '))
+
+if opção == 1:
+        x = calc_peso_folha()
+        print('O peso do papel é: {}'.format(x))
+else:
+    print('Em Construção')
+
+#print('Descrição: Papel {}, nas dimenssões {}mm x {}mm - {}g para quantidade: {} folhas.'.format(tipo, larg, comp, gram, qtdefls))
 
 
 
