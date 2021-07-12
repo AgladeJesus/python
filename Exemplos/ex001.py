fd0e04e5820c45edcd722bba291c9f7a070e7f28
AgladeJesus <agladejesus@gmail.com>
2021-07-12 17:32:27 -0400

    Update ex001.py

--- Exemplos/ex001.py
+++ Exemplos/ex001.py
@@ -9,20 +9,42 @@ print('''
 [5] CALCULAR MICRO-ONDULADO FACE SIMPLES
 ''')
 def calc_peso_folha():
-    larg = int(input('Largura: '))
-    comp = int(input('Comprimento: '))
-    gram = int(input('Gramatura: '))
 
     peso = ((larg/1000)*(comp/1000)*(gram/1000))
     return peso
 
+def calc_metroquadrado_folha():
+
+    metroQfl = ((larg / 1000) * (comp / 1000))
+
+    return metroQfl
+
 opção = int(input('O quer você deseja fazer: '))
 
 if opção == 1:
-        x = calc_peso_folha()
-        qtdefls = int(input('Qtde de folhas: '))
-        pesoTfls = x * qtdefls
-        print('O peso para essa quantidade de folhas é: {:.2f} quilos'.format(pesoTfls))
+    print('-='*20)
+    print('CONVERTER FOLHAS PARA QUILLOS')
+    print('-=' * 20)
+
+    larg = int(input('Largura: '))
+    comp = int(input('Comprimento: '))
+    gram = int(input('Gramatura: '))
+    vlrPct = float(input('Valor do pacote: '))
+    qtdePct = int(input('Qtde de folhas no pacote: '))
+
+    x = calc_peso_folha()
+    #y = calc_metroquadrado_folha()
+
+    qtdefls = int(input('Qtde de folhas: '))
+    pesoTfls = x * qtdefls
+    print('-=' * 20)
+    print('RESUMO DAS INFORMAÇÕES')
+    print('-=' * 20)
+    print('Peso da folha: '.format(x))
+    #print('Area da folha em m²: '.format(y))
+    print('Preço unitário da folha: '.format(vlrPct / qtdePct))
+    print('Preço do quilos: '.format())
+    print('O peso para essa quantidade de folhas é: {:.2f} quilos'.format(pesoTfls))
 else:
     print('Em Construção')
 
