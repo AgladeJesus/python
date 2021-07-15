d5a6e94495985a76496c09e07c89e51ffdeabc9e
AgladeJesus <agladejesus@gmail.com>
2021-07-15 06:22:31 -0400

    Update Desafio057.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio057.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio057.py
@@ -1,4 +1,4 @@
-sx = ''
-while sx != 'M' != 'F':
-    sx = str(input('Digite o seu sexo [M/F]: ')).upper()
-print('Acabou')
+sx = str(input('Digite o seu sexo [M/F]: ')).strip().upper() [0]
+while sx not in 'MnFf':
+    sx = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sx))
