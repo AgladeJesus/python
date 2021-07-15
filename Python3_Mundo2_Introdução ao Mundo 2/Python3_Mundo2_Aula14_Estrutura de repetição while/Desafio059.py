e48933a9cc75d81a93c4066adf0e006e0adcc765
AgladeJesus <agladejesus@gmail.com>
2021-07-15 05:54:15 -0400

    Create Desafio059.py

---
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio059.py
@@ -0,0 +1,9 @@
+nm1 = int(input('Digite o primeiro valor: '))
+nm2 = int(input('Digite o segundo valor: '))
+
+print('''
+[ 1 ] Soma
+[ 2 ] multiplicar
+[ 3 ] maior
+[ 4 ] novos números
+[ 5 ] sair do programa''')
