6b26a27e7b4bfd4cec9af84004095c9f8098da56
AgladeJesus <agladejesus@gmail.com>
2021-07-16 04:23:25 -0400

    Update Desafio058.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio058.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio058.py
@@ -1,13 +1,13 @@
 from random import randint
 from time import sleep
+rdpc = randint(1, 10)
 nm = 0
-rdpc = 999
 cont = 0
 print('-=-' * 20)
 print('Vou pensar em um número entre 1 e 10. Tente descobrir...')
 print('-=-' * 20)
+print(rdpc)
 while nm != rdpc:
-    rdpc = randint(1, 10)
     nm = int(input('Em qual número eu pensei? '))
     print('PROCESSANDO...')
     sleep(0.3)
