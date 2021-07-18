bfa449e5e7e2a7652ab04c9301cd68e83ae28aa7
AgladeJesus <agladejesus@gmail.com>
2021-07-18 04:25:53 -0400

    Update Desafio061.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio061.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio061.py
@@ -15,18 +15,6 @@ print('-='*20)
 
 primeiro = int(input('Primeiro termo: '))
 razão = int(input('Razão: '))
-termo = primeiro
-cont = 1
-while cont <= 10:
-    print('{} -> '.format(termo), end='')
-    termo += razão
-    cont += 1
-print('FIM!')
-
-print('\n\n')
-
-primeiro = int(input('Primeiro termo: '))
-razão = int(input('Razão: '))
 cont = 1
 while cont <= 10:
     print('{} -> '.format(primeiro), end='')
