a097b511e19cc7bd60ffd36761318a536d7735c4
AgladeJesus <agladejesus@gmail.com>
2021-07-18 04:23:25 -0400

    Update Desafio061.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio061.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio061.py
@@ -8,3 +8,28 @@ dez = termo + (20 -1 ) * razão
 for c in range(termo, dez + razão, razão):
     print('{}'.format(c), end='-> ')
 print('ACABOU!')'''
+
+print('-='*20)
+print('PROGRESSÃO ARITMÉTICA')
+print('-='*20)
+
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razão
+    cont += 1
+print('FIM!')
+
+print('\n\n')
+
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(primeiro), end='')
+    primeiro += razão
+    cont += 1
+print('FIM!')
