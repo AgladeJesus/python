0cef67b293e8b193fb8adb77d9c99bca8dac1a28
AgladeJesus <agladejesus@gmail.com>
2021-07-18 06:10:44 -0400

    Create Desafio063.py

---
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula14_Estrutura de repetição while/Desafio063.py
@@ -0,0 +1,15 @@
+print('-='*20)
+print('SEQUÊNCIA DE FIBONACCI')
+print('-='*20)
+n = int(input('Quantos termos você quer mostrar? '))
+nm1 = 0
+nm2 = 1
+print('~'*20)
+print('{} -> {}'.format(nm1, nm2), end='')
+cont = 3
+while cont <= n:
+    nm3 = nm1 + nm2
+    print(' -> {}'.format(nm3), end='')
+    nm1 = nm2
+    nm2 = nm3
+    cont += 1
