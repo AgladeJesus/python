8fc568aa2b76ce5efc71835339092e2b6aa0820b
AgladeJesus <agladejesus@gmail.com>
2021-07-21 06:02:40 -0400

    Update Desafio066.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio066.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio066.py
@@ -1,9 +1,9 @@
-cont = 1
-nm = soma = 0
+cont = 0
+soma = 0
 while True:
-    nm = int(input(f'Digite o {cont}º número: '))
+    nm = int(input(f'Digite o {cont+1}º número: '))
     if nm == 999:
         break
     cont += 1
     soma += nm
-print(f'Foram lançados {cont-1} números, totalizando {soma}')
+print(f'Foram lançados {cont} números, totalizando {soma}')
