325057c5a1087f654837c66c3d6d75980d8d1023
AgladeJesus <agladejesus@gmail.com>
2021-07-22 15:17:20 -0400

    Create ex004.py

---
+++ Exemplos/ex004.py
@@ -0,0 +1,44 @@
+print('-='*10)
+print('SACOLA SDL')
+print('-='*10)
+
+def tipo_sacola():
+        print('''
+        [0] SAIR DESSE MODULO.
+        =========PEQUENA=========
+        [1] SL PEQ PRETA SDL
+        [2] SL PEQ ROSA SDL
+        [3] SL PEQ VERDE SDL
+        [4] SL PEQ BRANCA SDL
+        =========MÉDIA=========
+        [5] SL PEQ PRETA SDL
+        [6] SL PEQ ROSA SDL
+        [7] SL PEQ VERDE SDL
+        [8] SL PEQ BRANCA SDL
+        =========GRANDE=========
+        [9] SL PEQ PRETA SDL
+        [10] SL PEQ ROSA SDL
+        [11] SL PEQ VERDE SDL
+        [12] SL PEQ BRANCA SDL
+        =========MICRO=========
+        [13] SL PEQ PRETA SDL
+        [14] SL PEQ ROSA SDL
+        [15] SL PEQ VERDE SDL
+        [16] SL PEQ BRANCA SDL
+        =========NANO=========
+        [17] SL PEQ PRETA SDL
+        [18] SL PEQ ROSA SDL
+        [19] SL PEQ VERDE SDL
+        [20] SL PEQ BRANCA SDL
+        ''')
+def infSacolas()
+    
+
+while True:
+    tipo = int(input('Qual tipo de Sacola: '))
+    if tipo == 0:
+        break
+    elif tipo == 1:
+
+        print('SL PEQ PRETA SDL')
+
