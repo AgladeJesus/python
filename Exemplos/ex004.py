9385b1b3ffc2f594737780fa271c6cdc30101ac7
AgladeJesus <agladejesus@gmail.com>
2021-07-22 16:07:32 -0400

    Update ex004.py

--- Exemplos/ex004.py
+++ Exemplos/ex004.py
@@ -8,37 +8,37 @@ def tipo_sacola():
         =========PEQUENA=========
         [1] SL PEQ PRETA SDL
         [2] SL PEQ ROSA SDL
-        [3] SL PEQ VERDE SDL
+        [3] SL PEQ TIFANY SDL
         [4] SL PEQ BRANCA SDL
         =========MÉDIA=========
         [5] SL PEQ PRETA SDL
         [6] SL PEQ ROSA SDL
-        [7] SL PEQ VERDE SDL
+        [7] SL PEQ TIFANY SDL
         [8] SL PEQ BRANCA SDL
         =========GRANDE=========
         [9] SL PEQ PRETA SDL
         [10] SL PEQ ROSA SDL
-        [11] SL PEQ VERDE SDL
+        [11] SL PEQ  TIFANY SDL
         [12] SL PEQ BRANCA SDL
         =========MICRO=========
         [13] SL PEQ PRETA SDL
         [14] SL PEQ ROSA SDL
-        [15] SL PEQ VERDE SDL
+        [15] SL PEQ  TIFANY SDL
         [16] SL PEQ BRANCA SDL
         =========NANO=========
         [17] SL PEQ PRETA SDL
         [18] SL PEQ ROSA SDL
-        [19] SL PEQ VERDE SDL
+        [19] SL PEQ  TIFANY SDL
         [20] SL PEQ BRANCA SDL
         ''')
 def infSacolas()
-    
+
 
 while True:
     tipo = int(input('Qual tipo de Sacola: '))
     if tipo == 0:
         break
     elif tipo == 1:
-
+        
         print('SL PEQ PRETA SDL')
 
