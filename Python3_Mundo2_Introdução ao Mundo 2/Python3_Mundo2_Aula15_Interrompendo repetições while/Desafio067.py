0a0584cf4baf953d478565c074cd65b506f43f92
AgladeJesus <agladejesus@gmail.com>
2021-07-23 04:22:16 -0400

    Update Desafio067.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio067.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio067.py
@@ -4,9 +4,10 @@ print('-='*20)
 nm1 = 0
 while True:
     nm1 = int(input('Você deseja a TABUADA de qual número: '))
-    if nm1 < 0:
+    if nm1 <= 0:
         break
     cont = 1
     while cont <= 10:
         print(f'{nm1} x {cont} = {nm1*cont}')
         cont += 1
+        
