5c2b0f151ab6dea9c29eb688ea9a0df7e05c06f0
AgladeJesus <agladejesus@gmail.com>
2021-07-23 05:59:04 -0400

    Update Desafio068.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio068.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio068.py
@@ -1,7 +1,7 @@
 from random import randint
 
 print('Olá, caro jogador, sou o computador, e quero dispultar com você no Impar/Par.')
-
+v = 0
 while True:
     jogador = int(input('Digita um número de 1 a 10: '))
     cp = randint(0, 10)
@@ -9,19 +9,25 @@ while True:
     tipo = ' '
     while tipo not in 'PI':
         tipo = str(input('Par ou ìmpar: [P/I]: ')).strip().upper()[0]
-    print(f'Você jogou {jogador} e o computador {cp}, o total é {total}')
+    print(f'Você jogou {jogador} e o computador {cp}, o total é {total}', end=' ')
     print('DEU PAR' if total % 2 == 0 else 'DEU ÍMPAR')
     if tipo == 'P':
         if total % 2 == 0:
             print('Você VENCEU!!')
+            v += 1
         else:
             print('Você PERDEU!!')
             break
     elif tipo == 'I':
         if total % 2 == 1:
             print('Você VENCEU!!')
+            v += 1
         else:
             print('Você PERDEU!!')
             break
+if v > 0:
+    print(f'Você tem {v} votórias')
+else:
+    print(' ')
 print('Vamos jogar novamente...')
 
