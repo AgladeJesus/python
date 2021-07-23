2a69702230e16f533460e68fad794611a1faea2b
AgladeJesus <agladejesus@gmail.com>
2021-07-23 06:16:23 -0400

    Create Desafio069.py

---
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio069.py
@@ -0,0 +1,8 @@
+print('-='*20)
+print('CADASTRO DE PESSOAS')
+print('-='*20)
+
+while True:
+    idade = int(input('Qual idade: ')
+    while sexo not in 'MF':
+        sexo = str(input(Digite o sexo [M/F]: ))
