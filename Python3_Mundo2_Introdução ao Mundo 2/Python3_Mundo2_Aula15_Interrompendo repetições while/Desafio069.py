20cff2a211c8de80d8e50d2cfe5d28f907ba0446
AgladeJesus <agladejesus@gmail.com>
2021-07-23 06:36:15 -0400

    Update Desafio069.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio069.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio069.py
@@ -3,6 +3,10 @@ print('CADASTRO DE PESSOAS')
 print('-='*20)
 
 while True:
-    idade = int(input('Qual idade: ')
-    while sexo not in 'MF':
-        sexo = str(input(Digite o sexo [M/F]: ))
+  #  idade = int(input('Qual idade: ')
+    sx = ' '
+    while sx not in 'mf':
+        sx = str(input('Digite o sexo [M/F]: '))
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou ìmpar: [P/I]: ')).strip().upper()[0]
