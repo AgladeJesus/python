f32b8faf7c4da0b1d6c62263080e2332e14dd372
AgladeJesus <agladejesus@gmail.com>
2021-07-27 05:31:44 -0400

    Update Desafio070.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio070.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio070.py
@@ -13,9 +13,11 @@ while True:
         maior1000 += 1
     if cont == 1:
         menorpreco = preco
+        barato = prod
     else:
         if preco < menorpreco:
             menorpreco = preco
+            barato = prod
     status = ' '
     while status not in 'SN':
         status = str(input('Quer Continuar? [S/N]: ')).upper().strip()[0]
@@ -26,7 +28,7 @@ print('FIM DO PROGRAMA')
 print('-'*20)
 print(f'O total da compra foi R$ {precoT}')
 print(f'Temos {maior1000} produto custando mais de R$ 1000.00')
-print(f'O produto mais barato foi {prod} que custa R${menorpreco}')
+print(f'O produto mais barato foi {barato} que custa R${menorpreco}')
 
 
 
