0800bd4183223ab4cc60cc4238fea92aed009d2b
AgladeJesus <agladejesus@gmail.com>
2021-07-27 11:33:27 -0400

    Update Desafio040.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio040.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio040.py
@@ -4,9 +4,11 @@ print('-='*40)
 
 nm1 = float(input('Digite a sua primeira nota: '))
 nm2 = float(input('Digite a sua segunda nota: '))
+nm3 = float(input('Digite a sua terceira nota: '))
+nm4 = float(input('Digite a sua quarto nota: '))
 
-media = (nm1+nm2)/2
-print('As notas do aluno são: {} e {}, resultando na seguinte média: {:.2f}'.format(nm1, nm2, media))
+media = (nm1+nm2+nm3+nm4)/4
+print(f'As notas do aluno são: {nm1}, {nm2},{nm3} e {nm4} resultando na seguinte média: {media:.2f}')
 if media < 5.0:
     print('REPROVADO')
 elif (media >= 5.0) and (media <= 6.9):
