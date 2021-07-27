746b494bfc4a5768ca39d0e75edb6f95ea4d7cb0
AgladeJesus <agladejesus@gmail.com>
2021-07-27 10:11:14 -0400

    Update aula12.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/aula12.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/aula12.py
@@ -1,12 +1,16 @@
-nome = str(input('Qual é su nome: '))
-if nome == 'Alexandre':
-    print('Que nome bonito!')
-elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
-    print('Seu nome é bem popular no Brasil.')
-elif nome in 'Ana Cláudia Jéssica Juliana':
-    print('Belo nome feminino')
-elif nome == 'Arthur' or 'Gleiciane' or 'Lohanny':
-    print('Esse nome compõem a família Agla de Jesus')
-else:
-    print('Seu nome é bem normal.')
+while True:
+    nome = ' '
+    nome = str(input('Qual é su nome: ')).capitalize().strip()
+    if nome == 'Alexandre':
+        print('Que nome bonito!')
+    elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+        print('Seu nome é bem popular no Brasil.')
+    elif nome in 'Ana Cláudia Jéssica Juliana':
+        print('Belo nome feminino')
+    elif nome == 'Arthur' or nome == 'Gleiciane' or nome == 'Lohanny':
+        print('Esse nome compõem a família Agla de Jesus')
+    elif nome != 'Exit':
+            print('Seu nome é bem normal.')
+    else:
+        break
 print('Tenha um bom dia,'.format(nome))
