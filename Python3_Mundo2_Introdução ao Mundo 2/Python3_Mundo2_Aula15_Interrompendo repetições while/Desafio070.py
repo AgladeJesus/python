0ae0ba07cccd50536457c2d0edeb9ea12f992f42
AgladeJesus <agladejesus@gmail.com>
2021-07-27 04:30:44 -0400

    Update Desafio070.py

---
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio070.py
@@ -0,0 +1,11 @@
+print('-'*20)
+print('LOJA SUPER BARATÃO')
+print('-'*20)
+
+while status != 'S':
+    prod = str(input('Nome do Produto: ')).upper().strip()
+    preco = float(input('Preço R$ '))
+    status = str(input('Quer Continuar? [S/N]: ')).upper().strip()[0]
+    if status == 'N':
+        break
+
