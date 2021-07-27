a47d44c0dd3348d0282b51400fd3ae8c4af4cea6
AgladeJesus <agladejesus@gmail.com>
2021-07-27 10:39:02 -0400

    Update Desafio037.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio037.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio037.py
@@ -14,8 +14,8 @@ numOct = oct(nm)
 numHex = hex(nm)
 
 if base == 1:
-    print('Número binário é: {}'.format(numBin [2:]))
+    print(f'Número binário é: {numBin [2:]}')
 elif base == 2:
-    print('Númenro octal é: {}'.format(numOct [2:]))
+    print(f'Númenro octal é: {numOct [2:]}')
 else:
-    print('Númenro Hexadecimal é: {}'.format(numHex [2:]))
+    print(f'Númenro Hexadecimal é: {numHex [2:]}')
