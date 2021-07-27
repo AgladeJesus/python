efe9ac0d9fd6bfd275cd188bb95fd4b3f590223d
AgladeJesus <agladejesus@gmail.com>
2021-07-27 05:29:41 -0400

    Update Desafio070.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio070.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula15_Interrompendo repetições while/Desafio070.py
@@ -1,12 +1,33 @@
 print('-'*20)
 print('LOJA SUPER BARATÃO')
 print('-'*20)
-status = ' '
-while status == 'S':
+
+precoT = maior1000 = menorpreco = cont = 0
+barato = ' '
+while True:
     prod = str(input('Nome do Produto: ')).upper().strip()
     preco = float(input('Preço R$ '))
-    status = str(input('Quer Continuar? [S/N]: ')).upper().strip()[0]
+    cont += 1
+    precoT += preco
+    if preco > 1000:
+        maior1000 += 1
+    if cont == 1:
+        menorpreco = preco
+    else:
+        if preco < menorpreco:
+            menorpreco = preco
+    status = ' '
+    while status not in 'SN':
+        status = str(input('Quer Continuar? [S/N]: ')).upper().strip()[0]
     if status == 'N':
         break
+print('-'*20)
+print('FIM DO PROGRAMA')
+print('-'*20)
+print(f'O total da compra foi R$ {precoT}')
+print(f'Temos {maior1000} produto custando mais de R$ 1000.00')
+print(f'O produto mais barato foi {prod} que custa R${menorpreco}')
+
+
 
 
