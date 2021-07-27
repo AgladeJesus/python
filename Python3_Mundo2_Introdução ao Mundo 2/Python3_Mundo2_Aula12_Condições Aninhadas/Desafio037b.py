5a7b819113b5c8a5bf5a934d50d9485516b1bd40
AgladeJesus <agladejesus@gmail.com>
2021-07-27 11:19:51 -0400

    Update Desafio037b.py

--- Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio037b.py
+++ Python3_Mundo2_Introdução ao Mundo 2/Python3_Mundo2_Aula12_Condições Aninhadas/Desafio037b.py
@@ -3,8 +3,8 @@ print('{:^40}'.format('CONVERSÃO DE BASES NUMERICAS'))
 print('='*40)
 while True:
     nm = int(input('Digite um número para ser convertido: '))
-    '''    if nm == 0:
-        break'''
+    if nm == 0:
+        break
     print(f'Número binário é: {bin(nm)[2:]}')
     print(f'Númenro octal é: {oct(nm)[2:]}')
     print(f'Númenro Hexadecimal é: {hex(nm)[2:]}')
